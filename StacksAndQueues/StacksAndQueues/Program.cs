c5e482b2450503009bfd1147a13aca8083e5f3ea
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-26 20:59:20 +0530

    Add: Peek and pop from Stack

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -11,6 +11,10 @@ namespace StacksAndQueues
             stack.push(30);
             stack.push(56);
             stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.IsEmpty();
+            stack.Display();
         }
     }
 }
--- StacksAndQueues/StacksAndQueues/Stacks.cs
+++ StacksAndQueues/StacksAndQueues/Stacks.cs
@@ -41,5 +41,32 @@ namespace StacksAndQueues
                 Console.WriteLine();
             }
         }
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
     }
 }
