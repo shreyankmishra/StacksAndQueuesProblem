9ba459cad73d10a5eef224bbbd75c228fa664d78
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-26 20:52:49 +0530

    Add: Creating a Stack

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stacks stack = new Stacks();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ StacksAndQueues/StacksAndQueues/Stacks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Stacks
+    {
+        private Node top;
+        public Stacks()
+        {
+            this.top = null;
+        }
+        public void push(int item)
+        {
+            Node node = new Node(item);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
