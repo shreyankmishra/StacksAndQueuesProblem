f5bd019d1c27c8f228b043716a5531bd8b45e383
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-26 21:08:53 +0530

    Add: Creating a Queue

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -6,15 +6,20 @@ namespace StacksAndQueues
     {
         static void Main(string[] args)
         {
-            Stacks stack = new Stacks();
-            stack.push(70);
-            stack.push(30);
-            stack.push(56);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.IsEmpty();
-            stack.Display();
+            //Stacks stack = new Stacks();
+            //stack.push(70);
+            //stack.push(30);
+            //stack.push(56);
+            //stack.Display();
+            //stack.Peek();
+            //stack.Pop();
+            //stack.IsEmpty();
+            //stack.Display();
+            Queues queue = new Queues();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
